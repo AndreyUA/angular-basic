e3236598af130c68b0a735bf0670e6a0c570b30a
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-08 14:58:12 +0300

    output, emitting events

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -9,6 +9,8 @@ export class PostComponent implements OnInit {
   constructor() {}
 
   @Input('img') postImg: string = '';
+
+  @Output() imgSelected = new EventEmitter<string>();
 
   ngOnInit(): void {}
 }
